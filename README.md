25d556de91786ea65e3e79de1186268a7d1155bc
Bán Dénes <mr@zealot.hu>
2018-09-10 00:19:44 +0200

    Initial CAD model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# polygon
+# Polygon
+
+A minimalistic, multi-plane ergonomic keyboard. Blog post coming in the future, but in the meantime you can check out my [prototype post on reddit](http://redd.it/9aam0u).
---
+++ cad/polygon.py
@@ -0,0 +1,550 @@
+import math
+
+# OpenSCAD modeling API
+from solid import *
+from solid.utils import *
+
+
+#
+# PARAMETERS
+#
+
+# Suffix meanings:
+# - w = width
+# - h = height
+# - d = depth
+# - x }
+# - y } starting coordinates
+# - z }
+# - t = thickness
+# - m = margin
+# - p = padding
+
+# Shared stuff
+
+screw_size = 2.5
+hole_w = 14
+hole_h = 14
+padding = 5
+margin = 3
+ledge = 2
+plate_t = 1.5
+wall_t = 5
+top_t = 8
+mid_t = 8
+
+a_lot = 1000 # Symbolic vars for cutting planes and such
+a_little = 200
+
+# Keywell
+
+rows = 3
+staggers = [0, 12, 5, -6, -2]
+pinky_angle = 5
+cover_t = 2
+
+# Thumbfan
+
+thumb_plane_start = 55
+keycap_h = 18
+alphakey_w = 18
+thumbkey_w = 22.5
+thumb_length = 65
+thumbfan_h = 35
+
+# Case
+
+bottom_t = 28
+bottom_m = 5
+hatch_w = 8
+corridor = 35
+socket_d = 5
+socket_margin = 3
+
+# Internals
+
+ctrl_w = 51 # ctrl = microcontroller
+ctrl_h = 22.9
+ctrl_d = 7.1
+ctrl_margin_x = 8
+ctrl_margin_y = 3
+ctrl_standoff = 8
+ctrl_screw_p = 2.5
+battery_w = 36
+battery_h = 30
+battery_d = 5
+
+# Assembly
+
+tenting = 45
+total_length = 120
+halves_p = 30
+
+
+
+
+
+#
+# COMPUTED VALUES
+#
+
+cols = len(staggers)
+
+_current = 0
+highest_point = 0
+highest_index = 0
+for i, stagger in enumerate(staggers):
+    _current += stagger
+    if _current > highest_point:
+        highest_point = _current
+        highest_index = i
+
+col_h = rows * hole_h + (rows - 1) * padding + 2 * margin
+
+pinky_sin = math.sin(math.radians(pinky_angle))
+pinky_extra_w = ceil(col_h * pinky_sin)
+pinky_extra_h = ceil(hole_w * pinky_sin)
+
+width = cols * hole_w + (cols - 1) * padding + 2 * margin + pinky_extra_w + 2 * wall_t
+height = col_h + highest_point + pinky_extra_h + 2 * wall_t
+
+# Assuming the thumb length parameter and the keycap sizes are available,
+# we can calculate the exact angle between the thumb keys so that they're
+# on an arc with 1mm between the keycaps, using 2 right triangles.
+
+# The first right triangle is given by A) the base of the thumb,
+# B) the bottom center of the keycap, and C) the bottom right corner of
+# the keycap. AB is thumb_length - (keycap_h / 2), and BC is thumbkey_w,
+# so AC is calculable. Then the angle BAC is asin(BC / AC).
+
+# The second right triangle is A), C), and D), where D) is the midpoint of
+# the 1mm segment between the corners of the 2 keycaps. CD is orthogonal to
+# AD, and since AC is known from before and AD is 0.5, the CAD angle is
+# asin(CD / AC)
+
+# Finally, the desired rotation is 2 * (BAC + CAD)
+
+ac = math.sqrt( (thumbkey_w / 2)**2 + (thumb_length - (keycap_h / 2))**2 )
+bac = math.asin( (thumbkey_w / 2) / ac)
+cad = math.asin(0.5 / ac)
+thumb_angle = math.degrees(2 * (bac + cad))
+
+hatch_h = height - (width - thumb_plane_start) + 8
+
+ctrl_x = width - ctrl_w - hatch_w - ctrl_margin_x
+ctrl_y = height - ctrl_h - wall_t - ledge + sum(staggers[highest_index+1:]) - ctrl_margin_y
+battery_x = ctrl_x + (ctrl_w - battery_w) / 2
+battery_y = ctrl_y - (battery_h - ctrl_h)
+
+
+
+#
+# PRESENTATION
+#
+
+default_alpha = 1
+colors = {
+    'keywell_top': [1, 0, 0],
+    'keyplate': [.8, .8, .8],
+    'keywell_mid': [0, 1, 0],
+    'keywell_cover': [0, 0, .5],
+    'keywell_upper_bottom': [0, 0, 1],
+    'keywell_lower_bottom': [1, 1, 0],
+    'keywell_base': [1, 0, 1],
+    'thumbfan_top': [1, .5, 0],
+    'thumbplate': [.8, .8, .8],
+    'thumbfan_mid': [0, 1, 1],
+    'controller': [0, 0, 0],
+    'battery': [1, 1, 1],
+    'battery_plate': [.8, .8, .8],
+    'hatch': [.5, 0, 0],
+}
+
+def highlight(key, profile):
+    if key in colors:
+        c = colors[key]
+        if len(c) < 4:
+            c.append(default_alpha)
+        return color(c)(render()(profile))
+    else:
+        return render()(profile)
+
+
+
+
+#
+# SHARED PARTS
+#
+
+def hole():
+    return square([hole_w, hole_h])
+
+def screw_hole():
+    return circle(d=screw_size, segments=a_lot)
+
+def corner_cut(side):
+    corner = polygon([
+        [width - side, 0],
+        [width, 0],
+        [width, side]
+    ])
+    return down(a_lot / 2)(linear_extrude(a_lot)(corner))
+
+def keywell_corner_cut():
+    return corner_cut(sum(staggers) + pinky_extra_h)
+    
+def thumb_plane_cut():
+    return corner_cut(width - thumb_plane_start)
+
+def bottom_cut():
+    return rotate([0, tenting, 0])(down(a_lot)(cube(a_lot * 2, center=True)))
+
+def upper_side_cut():
+    return down(a_lot / 2)(right(width)(cube(a_lot)))
+
+def lower_side_cut():
+    return rotate([0, tenting, 0])(right(total_length)(cube(a_lot)))
+
+def front_cut():
+    return down(a_lot / 2)(back(a_lot)(cube(a_lot)))
+
+
+
+
+#
+# KEYWELL
+#
+
+def column():
+    res = square(0)
+    for i in range(rows):
+        res += forward(i * (hole_h + padding))(hole())
+    return res
+
+def matrix():
+    res = square(0)
+    total_stagger = 0
+    for i, stagger in enumerate(staggers):
+        total_stagger += stagger
+        col = column()
+        if i is 0:
+            col = left(hole_w)(col)
+            col = rotate(pinky_angle)(col)
+            col = right(hole_w)(col)
+        col = forward(total_stagger)(col)
+        col = right(i * (hole_w + padding))(col)
+        res += col
+    return res
+
+def keyplate_contour():
+    inflation = 7
+    return offset(delta=-inflation)(offset(delta=margin + inflation)(matrix()))
+
+def adjust_keyplate_xy(plate):
+    plate = right(pinky_extra_w + margin + wall_t)(plate)
+    plate = forward(pinky_extra_h + margin + wall_t)(plate)
+    return plate
+
+def keyplate_screw_holes():
+    h1 = screw_hole()
+    h1 = right(hole_w)(h1)
+    h1 = forward(rows * hole_h + (rows - 1) * padding - socket_margin)(h1)
+
+    h2 = screw_hole()
+    h2 = right(hole_w + padding / 2)(h2)
+    h2 = forward(staggers[1] + socket_margin)(h2)
+
+    h3 = screw_hole()
+    h3 = right((cols - 2) * hole_w + (cols - 2.5) * padding)(h3)
+    h3 = forward(sum(staggers[:-2]) + socket_margin)(h3)
+
+    return adjust_keyplate_xy(h1 + h2 + h3)
+
+def keyplate_footprint():
+    plate = keyplate_contour() - matrix()
+    plate = adjust_keyplate_xy(plate)
+    plate -= keyplate_screw_holes()
+    return plate
+
+def keyplate():
+    plate = linear_extrude(plate_t)(keyplate_footprint())
+    plate = up(mid_t)(plate)
+    return highlight('keyplate', plate)
+
+def keywell_top():
+    profile = square([width, height])
+    profile -= adjust_keyplate_xy(keyplate_contour())
+    profile = linear_extrude(top_t)(profile)
+    profile -= keywell_corner_cut()
+    profile = up(mid_t)(profile)
+    return highlight('keywell_top', profile)
+
+def keywell_mid():
+    sub = adjust_keyplate_xy(offset(delta=-ledge)(keyplate_contour()))
+    profile = square([width, height]) - sub
+    profile = linear_extrude(mid_t)(profile)
+    profile -= keywell_corner_cut()
+    return highlight('keywell_mid', profile)
+
+def keywell_cover():
+    profile = cube([cover_t, a_little, a_little])
+    profile = up(top_t + mid_t)(profile)
+    profile = map_to_thumb_plane(profile)
+
+    profile -= front_cut()
+    profile -= upper_side_cut()
+    profile -= keywell_corner_cut()
+
+    return highlight('keywell_cover', profile)
+
+
+
+
+#
+# THUMBFAN
+#
+
+def adjust_thumb_x(fan):
+    return right(wall_t + margin + (thumbkey_w - alphakey_w) / 2)(fan)
+
+def thumb_holes_helper(profile, shift):
+    first = left(shift)(profile)
+    second = left(shift)(profile)
+    second = forward(thumb_length)(second)
+    second = rotate(-thumb_angle)(second)
+    second = back(thumb_length)(second)
+    
+    fan = first + second
+    fan = right(shift)(forward(thumbfan_h)(fan))
+    fan = adjust_thumb_x(fan)
+    return fan
+
+def thumb_holes():
+    return right((thumbkey_w - alphakey_w) / 2)(thumb_holes_helper(hole(), hole_w / 2))
+
+def thumb_footprint():
+    profile = square([hole_w + (thumbkey_w - alphakey_w), hole_h])
+    shift = hole_w / 2 + (thumbkey_w - alphakey_w) / 2
+    return thumb_holes_helper(profile, shift)
+
+def thumbplate_contour():
+    inflation = 10
+    return offset(delta=-inflation)(offset(delta=margin + inflation)(thumb_footprint()))
+
+def map_to_thumb_plane(plate):
+    plate = rotate([0, 90, -45])(plate)
+    plate = right(thumb_plane_start)(plate)
+    return plate
+
+def thumbplate_screw_hole():
+    h = screw_hole()
+    h = forward(thumb_length + hole_h * 0.65)(h)
+    h = rotate(-thumb_angle * 0.5)(h)
+    h = back(thumb_length - thumbfan_h)(h)
+    h = right(hole_w / 2 + (thumbkey_w - alphakey_w) / 2)(h)
+    h = adjust_thumb_x(h)
+    return h
+
+def thumbplate_footprint():
+    plate = thumbplate_contour() - thumb_holes()
+    plate -= thumbplate_screw_hole()
+    return plate
+
+def thumbplate():
+    plate = linear_extrude(plate_t)(thumbplate_footprint())
+    plate = up(mid_t)(plate)
+    plate = map_to_thumb_plane(plate)
+    return highlight('thumbplate', plate)
+
+def thumb_cut_around(profile):
+    return profile - bottom_cut() - upper_side_cut() - lower_side_cut() - front_cut()
+
+def thumbfan_top():
+    profile = square([a_lot / 2, a_lot / 2])
+    profile -= thumbplate_contour()
+    profile = linear_extrude(top_t)(profile)
+    profile = up(mid_t)(profile)
+    profile = map_to_thumb_plane(profile)
+    return highlight('thumbfan_top', thumb_cut_around(profile))
+
+def thumbfan_mid():
+    sub = offset(delta=-ledge)(thumbplate_contour())
+    profile = square([a_lot / 2, a_lot / 2]) - sub
+    profile = linear_extrude(mid_t)(profile)
+    profile = map_to_thumb_plane(profile)
+    profile -= hatch()
+    return highlight('thumbfan_mid', thumb_cut_around(profile))
+
+
+
+
+#
+# CASE
+#
+
+def corridor_cut():
+    c = cube([corridor, ctrl_y + ctrl_h + bottom_m, bottom_t])
+    c = right(width - corridor)(c)
+    c = down(bottom_t)(c)
+    return c
+
+def hatch():
+    profile = cube([hatch_w, hatch_h, a_little])
+    profile = right(width - hatch_w)(profile)
+    profile = forward(height - hatch_h)(profile)
+    profile = down(a_little)(profile)
+    profile -= lower_side_cut()
+    profile -= bottom_cut()
+
+    return highlight('hatch', profile)
+
+def keywell_bottom():
+    profile = square([width, height])
+
+    sub = square([ctrl_w, battery_h])
+    sub = right(ctrl_x)(sub)
+    sub = forward(battery_y)(sub)
+    sub = offset(delta=bottom_m)(sub)
+    profile -= sub
+
+    profile = linear_extrude(bottom_t)(profile)
+    profile = down(bottom_t)(profile)
+    profile -= thumb_plane_cut()
+    profile -= bottom_cut()
+    profile -= corridor_cut()
+    profile -= hatch()
+
+    holes = keyplate_screw_holes()
+    holes = linear_extrude(socket_d)(holes)
+    holes = down(socket_d)(holes)
+    profile -= holes
+
+    return highlight('keywell_lower_bottom', profile)
+
+def keywell_base():
+    profile = cube([width, height, a_lot])
+    profile = down(bottom_t + a_lot)(profile)
+    profile -= thumb_plane_cut()
+    profile -= bottom_cut()
+    profile -= lower_side_cut()
+    profile -= hatch()
+
+    holes = ctrl_holes()
+    holes = linear_extrude(socket_d)(holes)
+    holes = down(bottom_t + socket_d)(holes)
+    profile -= holes
+
+    thumb_hole = thumbplate_screw_hole()
+    thumb_hole = linear_extrude(socket_d)(thumb_hole)
+    thumb_hole = down(socket_d)(thumb_hole)
+    thumb_hole = map_to_thumb_plane(thumb_hole)
+    profile -= thumb_hole
+
+    return highlight('keywell_base', profile)
+
+
+
+
+#
+# INTERNALS
+#
+
+def ctrl_holes():
+    csp = ctrl_screw_p
+    holes = right(csp)(forward(csp)(screw_hole()))
+    holes += right(csp)(forward(ctrl_h - csp)(screw_hole()))
+    holes += right(ctrl_w - csp)(forward(csp)(screw_hole()))
+    holes += right(ctrl_w - csp)(forward(ctrl_h - csp)(screw_hole()))
+    holes = right(ctrl_x)(holes)
+    holes = forward(ctrl_y)(holes)
+    return holes
+
+def ctrl_footprint():
+    c = square([ctrl_w, ctrl_h])
+    c = right(ctrl_x)(c)
+    c = forward(ctrl_y)(c)
+    c -= ctrl_holes()
+    return c
+
+def ctrl():
+    c = linear_extrude(ctrl_d)(ctrl_footprint())
+    c = down(bottom_t - plate_t - ctrl_standoff)(c)
+    return highlight('controller', c)
+
+def battery_footprint():
+    b = square([battery_w, battery_h])
+    b = right(battery_x)(b)
+    b = forward(battery_y)(b)
+    return b
+
+def battery():
+    b = linear_extrude(battery_d)(battery_footprint())
+    b = down(bottom_t - plate_t)(b)
+    return highlight('battery', b)
+
+def battery_plate_footprint():
+    return battery_footprint() + ctrl_footprint()
+
+def battery_plate():
+    p = linear_extrude(plate_t)(battery_plate_footprint())
+    p = down(bottom_t)(p)
+    return highlight('battery_plate', p)
+
+
+
+
+#
+# ASSEMBLY
+#
+
+def left_half(explode = 0):
+    up_parts = [
+        keywell_base(),
+        keywell_bottom(),
+        battery_plate(),
+        battery(),
+        ctrl(),
+        keywell_mid(),
+        keywell_cover(),
+        keyplate(),
+        keywell_top(),
+    ]
+
+    right_parts = [
+        hatch(),
+        thumbfan_mid(),
+        thumbplate(),
+        thumbfan_top(),
+    ]
+
+    half = cube(0)
+    for i, part in enumerate(up_parts):
+        part = rotate([0, -tenting, 0])(part)
+        half += up(i * explode)(part)
+
+    for i, part in enumerate(right_parts):
+        part = rotate([0, -tenting, 0])(part)
+        half += right((i + 1) * explode)(part)
+
+    return half
+
+def right_half():
+    return right(2 * total_length + halves_p)(
+        mirror([1, 0, 0])(
+            left_half()
+        )
+    )
+
+
+
+
+#
+# RENDERING
+#
+
+scad_render_to_file(left_half() + right_half(), 'polygon.scad')
+scad_render_to_file(left_half(30), 'polygon_exploded.scad')
+
+scad_render_to_file(keyplate_footprint(), 'polygon_keyplate.scad')
+scad_render_to_file(thumbplate_footprint(), 'polygon_thumbplate.scad')
+scad_render_to_file(battery_plate_footprint(), 'polygon_battery_plate.scad')
