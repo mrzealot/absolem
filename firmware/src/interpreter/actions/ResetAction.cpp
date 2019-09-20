ee2f2ce09467748ef7196a3e7a9c0082977a8617
Bán Dénes <mr@zealot.hu>
2019-09-20 10:23:32 +0200

    Extract firmware to its own repo

--- firmware/src/interpreter/actions/ResetAction.cpp
+++
@@ -1,9 +0,0 @@
-#include "ResetAction.h"
-
-namespace absolem {
-
-    void ResetAction::operator()(Interpreter& interpreter) {
-        interpreter.getController()->reset();
-    }
-    
-} // namespace absolem
